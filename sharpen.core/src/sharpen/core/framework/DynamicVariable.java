63366391b1ea47df2591b63a14394da59560a626
rodrigob <rodrigob@915b37e7-0715-0410-8bc3-b14b59444072>
2008-10-24 12:43:11 +0000

    DCF-27 - removed unnecessary code - inside a enhanced for statement, the iterator expression has already been determined to have an iterator method, coerce is not really necessary
    git-svn-id: https://source.db4o.com/db4o/trunk/sharpen@12003 915b37e7-0715-0410-8bc3-b14b59444072

--- sharpen.core/src/sharpen/core/framework/DynamicVariable.java
+++ sharpen.core/src/sharpen/core/framework/DynamicVariable.java
@@ -24,58 +24,6 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 package sharpen.core.framework;
 
 public class DynamicVariable<T> {
-	
-	private static final class SingleVariableCapture<T> implements DynamicVariableCapture {
-
-		private final T _value;
-		private final DynamicVariable<T> _variable;
-
-	    private SingleVariableCapture(DynamicVariable<T> variable, T value) {
-	    	_variable = variable;
-		    _value = value;
-	    }
-
-	    @Override
-	    public <TRESULT> TRESULT run(Producer<TRESULT> producer) {
-	    	return _variable.using(_value, producer);
-	    }
-
-	    @Override
-	    public DynamicVariableCapture combine(final DynamicVariableCapture context) {
-	    	return CombinedVariableCapture.combine(this, context);
-	    }
-    }
-	
-	private static final class CombinedVariableCapture implements DynamicVariableCapture {
-		
-		public static DynamicVariableCapture combine(DynamicVariableCapture head, DynamicVariableCapture tail) {
-			if (head == tail) return head;
-	    	return new CombinedVariableCapture(head, tail);
-	    }
-		
-        private final DynamicVariableCapture _head;
-		private final DynamicVariableCapture _tail;
-
-        private CombinedVariableCapture(DynamicVariableCapture head, DynamicVariableCapture tail) {
-	        _head = head;
-	        _tail = tail;
-        }
-
-        @Override
-        public DynamicVariableCapture combine(DynamicVariableCapture context) {
-        	return combine(this, context);
-        }
-
-		@Override
-        public <T> T run(final Producer<T> producer) {
-            return _head.run(new Producer<T>() {
-        		@Override
-                public T produce() {
-        			return _tail.run(producer);
-                }
-            });
-        }
-    }
 
 	private T _value;
 	
@@ -106,9 +54,4 @@ public class DynamicVariable<T> {
 		});
 		return result.value;
 	}
-
-	public DynamicVariableCapture using(final T value) {
-		return new SingleVariableCapture(this, value);
-    }
-	
 }

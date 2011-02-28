2d64f6d60ae1122bf4aade4bc525ef9bd5369886
Lluis Sanchez Gual <lluis@novell.com>
2011-02-28 11:45:31 +0100

    Fix cast exception. findDeclaringNode may return an EnumDeclaration.

--- sharpen.core/src/sharpen/core/internal/MappingsImpl.java
+++ sharpen.core/src/sharpen/core/internal/MappingsImpl.java
@@ -205,7 +205,7 @@ public class MappingsImpl implements Mappings {
 		ITypeBinding declaringClassBinding = binding.getDeclaringClass();
 		if (declaringClassBinding.isAnonymous()) return false;
 		
-		TypeDeclaration declaringClass = findDeclaringNode(declaringClassBinding);
+		AbstractTypeDeclaration declaringClass = findDeclaringNode(declaringClassBinding);
 		return declaringClass == null 
 					? false 
 					: isAnnotatedWith(declaringClass, SharpenAnnotations.SHARPEN_IGNORE_EXTENDS);
